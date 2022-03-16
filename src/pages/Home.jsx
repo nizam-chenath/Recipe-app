7e70383822e3a730a66a73e0d380ea1ee1020da7
nizam-chenath <nizamstudymajliz@gmail.com>
2022-03-16 22:15:26 +0530

    started new recipe app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Veggie/>
+        <Popular/>
+    </div>
+  )
+}
+
+export default Home
